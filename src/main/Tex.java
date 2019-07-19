ee552f83ff43ada258a50b22f0bf53706557e803
brian <swixee@gmail.com>
2019-07-18 22:28:05 -0400

    changed user loop to enum

--- src/main/Tex.java
+++ src/main/Tex.java
@@ -5,9 +5,10 @@ import java.nio.file.*;
 import java.util.*;
 
 public class Tex {	
-	static Charset utf8 = StandardCharsets.UTF_8;
+	private static Charset utf8 = StandardCharsets.UTF_8;
+
 	private static List<String> getHeader() {
-		List<String> header = new ArrayList<String>();
+		List<String> header = new ArrayList<>();
 		header.add("\\documentclass[12pt]{article}");
 		header.add("\\usepackage{amsmath,amssymb,theorem}");
 		header.add("\\topmargin -90pt \\oddsidemargin 0pt \\evensidemargin 0pt \\textwidth 6.5in \\textheight 10.25in");
@@ -17,16 +18,15 @@ public class Tex {
 	}
 	
 	private static List<String> getCloser() {
-		List<String> closer = new ArrayList<String>();
+		List<String> closer = new ArrayList<>();
 		closer.add("\\end{document}");
 		return closer;
 	}
-	
-	
+
 	//INPUT: a list of strings, which will be printed line by line, and a file name
 	public static void writeToFile(List<String> input, String fileName) {
 		try { 
-			List<String> output = new ArrayList<String>();
+			List<String> output = new ArrayList<>();
 			output.addAll(getHeader());
 			output.addAll(input);
 			output.addAll(getCloser());
